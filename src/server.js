f5588e154dd78ec23581b66aae482e2af69847f7
cleyxds <cleysonn@hotmail.com.br>
2021-07-01 17:49:22 -0300

    making this the producer

--- src/server.js
+++ src/server.js
@@ -1,19 +1,14 @@
 import express from 'express';
 import { Kafka, logLevel } from 'kafkajs';
-
 import routes from './routes';
 
 const SERVER_PORT = 3333
+export const POWER_TOPIC = 'POWER_SENSORS';
 
 const app = express();
 
-export const POWER_TOPIC = 'POWER_SENSORS';
-
-/**
- * Faz conexão com o Kafka
- */
 export const kafka = new Kafka({
-  clientId: 'ELECKTRA',
+  clientId: 'ELECKTRA-PRODUCER',
   brokers: ['localhost:9092'],
   logLevel: logLevel.NOTHING,
   retry: {
@@ -25,35 +20,32 @@ export const kafka = new Kafka({
 const producer = kafka.producer()
 const consumer = kafka.consumer({ groupId: 'POWER-GROUP-RECEIVER' })
 
-/**
- * Disponibiliza o producer para todas rotas
- */
 app.use((req, res, next) => {
   req.producer = producer;
 
   return next();
 });
 
-/**
- * Cadastra as rotas da aplicação
- */
 app.use(express.json());
 app.use(routes);
 
-async function run() {
+kafka_api(false).catch(console.error)
+
+async function kafka_api(debug) {
   await producer.connect()
-  await consumer.connect()
+  
+  if (debug) {
+    await consumer.connect()
 
-  await consumer.subscribe({ topic: POWER_TOPIC });
+    await consumer.subscribe({ topic: POWER_TOPIC });
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      const measurement = JSON.parse(String(message.value));
-      console.log(measurement);
-    },
-  });
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const measurement = JSON.parse(String(message.value));
+        console.log(measurement);
+      },
+    });
+  }
 
   app.listen(SERVER_PORT, () => {console.log(`Server ready on port ${SERVER_PORT}`)});
 }
-
-run().catch(console.error)
