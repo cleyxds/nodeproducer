156015b5c0904a60b60aa2b33a5cb06b3df74487
cleyxds <cleysonn@hotmail.com.br>
2021-10-01 00:22:06 -0300

    retry mechanism

--- src/kafka/producer.ts
+++ src/kafka/producer.ts
@@ -4,6 +4,11 @@ const KAFKA_BROKERS = process.env.KAFKA_BROKERS
 
 export const kafka = new Kafka({
   brokers: [KAFKA_BROKERS],
+  retry: {
+    factor: .2,
+    retries: 5,
+    multiplier: 2
+  },
   logLevel: logLevel.NOTHING
 })
 
